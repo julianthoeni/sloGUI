098da259f966aff637694aec4fb3922552c4f89c
Julian Thöni <julian.thoeni@gmail.com>
2023-02-26 19:14:34 +0100

    Port-config and design changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Main from "./Main";
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="/">
           SLO_GUI
         </a>
@@ -24,23 +24,23 @@ function App() {
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="home">
+              <a class="nav-link" href="home" >
                 Home <span class="sr-only"></span>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="slos">
-                SLO
+                SLOs
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="periods">
+                Periods
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="rules">
-                Rule
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="period">
-                Period
+                Rules
               </a>
             </li>
             <li class="nav-item">
--- client/src/Main.js
+++ client/src/Main.js
@@ -8,13 +8,14 @@ import Help from "./pages/Help";
 import Home from "./pages/Home";
 
 const Main = () => {
+  document.title = "SLO_GUI";
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/home" element={<Home />}></Route>
       <Route path="/slos" element={<SLOs />}></Route>
-      <Route path="/rules" element={<Rules />}></Route>
-      <Route path="/period" element={<Period />}></Route>
+      <Route path="/periods" element={<Period />}></Route>
+      <Route path="/rules" element={<Rules />}></Route>      
       <Route path="/about" element={<About />}></Route>
       <Route path="/help" element={<Help />}></Route>
     </Routes>
